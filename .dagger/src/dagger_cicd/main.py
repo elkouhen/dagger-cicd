9e6ea407a97327544f5cb1f60dd1a2eacb109fdf
melkouhen <113344536+melkouhen@users.noreply.github.com>
2025-06-02 15:02:11 +0200

    init

--- .dagger/src/dagger_cicd/main.py
+++ .dagger/src/dagger_cicd/main.py
@@ -13,33 +13,43 @@ class DaggerCicd:
                 .with_workdir("/src")\
                 .with_exec(["mvn", "clean", "install", "-DskipTests"])
 
-    def start_db(self, source_sql: dagger.File) -> dagger.Service:
+    @function
+    def start_db_2(self, source: dagger.Directory) -> dagger.Container:
 
         return dag.container()\
                 .from_("postgres:17.5-alpine")\
                 .with_env_variable("POSTGRES_USER", "postgres")\
                 .with_env_variable("POSTGRES_PASSWORD", "password")\
                 .with_exposed_port(5432)\
-                .with_mounted_file("/docker-entrypoint-initdb.d/init.sql", source_sql)\
+                .with_mounted_file("/docker-entrypoint-initdb.d/init.sql", dagger.Directory.file(source, "init.sql"))
+                
+    def start_db(self, source: dagger.Directory) -> dagger.Service:
+
+        return dag.container()\
+                .from_("postgres:17.5-alpine")\
+                .with_env_variable("POSTGRES_USER", "postgres")\
+                .with_env_variable("POSTGRES_PASSWORD", "password")\
+                .with_exposed_port(5432)\
+                .with_mounted_file("/docker-entrypoint-initdb.d/init.sql", dagger.Directory.file(source, "init.sql"))\
                 .as_service()
 
-    def start_app(self, db: dagger.Service,source: dagger.Directory, source_sql: dagger.File) -> dagger.Service:
+    def start_app(self, db: dagger.Service,source: dagger.Directory) -> dagger.Service:
         
         return dag.container().from_("maven:latest")\
                 .with_mounted_directory("/src", source)\
                 .with_workdir("/src")\
                 .with_service_binding("database", db)\
                 .with_exposed_port(8080)\
-                .with_exec(["mvn", "spring-boot:run"])\
-                .as_service()
+                .as_service(args=["mvn", "spring-boot:run"])
                 
     @function
-    def run(self, source: dagger.Directory, source_sql: dagger.File) -> dagger.Container :
+    def run(self, source: dagger.Directory) -> dagger.Container :
         
-        db = self.start_db(source_sql)
-        app = self.start_app(db, source, source_sql)
+        db = self.start_db(source)
+        app = self.start_app(db, source)
         
-        return dag.container().from_("alpine:latest").terminal(cmd=["ls"])
-                #.with_service_binding("database",db)\
-                #.with_service_binding("app",app)\
-                
+        return dag.container().from_("alpine:latest")\
+                .with_exec(["apk", "--no-cache", "add", "curl"])\
+                .with_service_binding("database",db)\
+                .with_service_binding("app",app)\
+                .terminal()
